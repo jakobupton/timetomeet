8e1dc7793522526d390c1cc78237e3bff2aaf92a
jakobupton <jakejupton@gmail.com>
2024-10-17 16:00:24 -0700

    create types for meeting and participant

--- main.ts
+++ main.ts
@@ -1,14 +1,44 @@
-import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router } from "https://deno.land/x/oak/mod.ts"; // latest oak version
 
 // need to point api.timetomeet.ca to reverse proxy to localhost:8000
 
-const meetings = new Map<string, any>();
-meetings.set("1", { // meeting id,
-  id: "1",
+type meeting = {
+  title: string,
+  startDate: string,
+  startTime: string,
+  participants: participant[],
+}
+
+type participant = {
+  id: number,
+  name: string,
+  email?: string,
+  password?: string, // optional only if a user opts in to edit their meeting times
+}
+
+const meetings = new Map<string, meeting>(); 
+// eventually this will be a database, but for now it's just a map
+
+meetings.set("1", { // reference to meeting
   title: "Meeting Name",
-  date: "2021-07-01", // date in ISO format
+  startDate: new Date("2024-09-22").toISOString(), // don't know if I like this
   startTime: "9:00", // initial time used to calculate availability
+  participants: [
+    {id: 1, name: "John Test", email: "test@timetomeet.ca", password: "password"},
+    {id: 2, name: "Jane Doe"},
+  ],
 });
+
+meetings.set("2", {
+  title: "Meeting 2",
+  startDate: new Date("2024-09-23").toISOString(),
+  startTime: "10:00",
+  participants: [
+    {id: 1, name: "Jakob Upton"},
+    {id: 2, name: "George Costanza"},
+  ],
+});
+
 
 const router = new Router();
 router
@@ -19,7 +49,7 @@ router
     if (meetings.has(context?.params?.id)) {
       context.response.body = meetings.get(context.params.id);
     }
-  });
+  })
 
 const app = new Application();
 app.use(router.routes());
